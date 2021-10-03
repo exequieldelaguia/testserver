20c600c211fda5707378598b943e6e9171138fe5
exequieldelaguia <exequieldelaguia@gmail.com>
2021-10-03 18:44:06 -0300

    agregado guardar datos en disco

--- app.js
+++ app.js
@@ -1,19 +1,59 @@
 const express = require('express')
+const fs = require('fs');
 const path = require("path");
-const app = express()
-const port = 80
+const app = express();
+const port = 80;
+const publicip = '190.19.188.166';
 
-let counter = 0;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const dataPath = path.join(__dirname + '/datos.json');
+
+let datosVacas = [];
+
+fs.readdir(process.cwd(), function (err, files) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+
+
+    if (!files.includes("datos.json")) {
+        fs.writeFileSync(dataPath, '[]');
+        return;
+    };
+
+    datosVacas = JSON.parse(fs.readFileSync(dataPath));
+});
+
+
+
+
+
+
+
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-app.get("/counter", (req, res) => {
-    counter++;
-    console.log(counter);
-    res.status(200).end()
+
+app.get('/vacas', (req, res) => {
+    res.status(200).json(datosVacas);
+})
+
+
+app.post('/agregarVaca', (req, res) => {
+    datosVacas.push(req.body);
+    console.log(datosVacas)
+    fs.writeFileSync(dataPath, JSON.stringify(datosVacas));
+    res.statusCode = 200;
+    res.end();
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Server listening listening at ${publicip}:${port}`)
 })
